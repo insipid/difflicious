3f240104999ddfd1496713cb04fff3df56c5f467
drew <drewamato@gmail.com>
2025-08-09 23:46:53 +0100

    Silly formatting tweak

--- cilicious.sh
+++ cilicious.sh
@@ -1,7 +1,7 @@
-# npm run test         && \
-npm run lint:js        && \
-uv run pytest          && \
-uv run mypy src/       && \
-uv run ruff check .    && \
-uv run black .         && \
+# npm run test      && \
+npm run lint:js     && \
+uv run pytest       && \
+uv run mypy src/    && \
+uv run ruff check . && \
+uv run black .      && \
 echo "Success"
